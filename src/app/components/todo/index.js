b0a28018d66b12f4596c3ef66bc9d474b3c19cc8
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-25 00:59:52 +0200

    Added todo module

---
+++ src/app/components/todo/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Christian on 4/30/2015.
+ */
+'use strict';
+
+module.exports = angular.module('todo', [
+    require('../panel').name
+]).config(['$stateProvider', function ($stateProvider) {
+        // States/Routes
+        $stateProvider
+            .state('panel.view.todos', {
+                url: '/todos',
+                role: 'public',
+                views: {
+                    '@panel': {
+                        controller: 'TodoCtrl as Todo',
+                        template: require('./content.html')
+                    }
+                }
+            });
+    }])
+    .controller('TodoCtrl', ['$state','auth',  function ($state, auth) {
+        if (!auth.isLoggedIn()){
+            $state.go('panel.view.login');
+        }
+    }]);
