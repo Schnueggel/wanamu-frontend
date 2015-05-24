b80d3dcc1a3afb29ae742a3c4c7c66ab2010e45e
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-25 01:04:53 +0200

    Home module is now responsible for imprint and about us page

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -9,31 +9,15 @@ module.exports = angular.module('home', [
         // States/Routes
         $stateProvider
             .state('panel.view.login', {
-                url: '/login',
+                url: '/imprint',
                 role: 'public',
                 views: {
                     '@panel': {
-                        controller: 'LoginCtrl as Login',
-                        template: require('./login.html')
+                        template: require('./imprint.html')
                     },
                     'header@panel': {
                         template: ''
                     }
                 }
             });
-    }])
-    .controller('LoginCtrl', ['$scope', 'auth',  function ($scope, auth) {
-        var Login = this;
-
-        Login.pattern = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i;
-
-        Login.form = {
-            submitted: false
-        };
-
-        Login.login = function(){
-            if ($scope.loginform.$valid){
-                auth.login(Login.form.username, Login.form.password);
-            }
-        }
     }]);
