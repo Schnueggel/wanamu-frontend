629354c2e71b0ba07932dffb908647e28526a3ed
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-25 01:08:01 +0200

    Created authentication service

--- src/app/services/auth.js
+++ src/app/services/auth.js
@@ -4,15 +4,96 @@
 
 module.exports = function(ngModule){
 
-    ngModule.factory('auth', ['constants', function(constants){
+    ngModule.factory('auth', ['$q', '$http', '$window','constants', function($q, $http, $window, constants){
 
-        var currentuser;
+        var currentuser =  $window.localStorage.getItem('user');
 
         return {
             login: function(username, password) {
-            },
-            logout: function() {
+                /**
+                 * @return {Promise}
+                 */
+                return $q(function(resolve, reject){
 
+                    $http.post(constants.loginurl, {
+                        username: username,
+                        password: password
+                    }).success(function(data){
+                        console.log(data);
+
+                        if (!angular.isObject(data) || !angular.isArray(data.data) || data.data.length !== 1 || !angular.isNumber(data.data[0].id)){
+                            reject({
+                                name: 'unkown',
+                                message: 'Invalid data received from server'
+                            });
+                            return;
+                        }
+
+                        currentuser = data.data[0];
+
+                        $window.localStorage.setItem('user', currentuser);
+
+                        resolve(currentuser);
+
+                    }).error(function(data) {
+                        if (data && data.error) {
+                            reject(data.error);
+                        } else {
+                            reject({
+                                name: 'unkown',
+                                message: 'Invalid data received from server'
+                            });
+                        }
+                    });
+                });
+            },
+            /**
+             * Logsout user
+             * @returns {Promise}
+             */
+            logout: function() {
+                return $q(function(resolve, reject){
+
+                    // ==========================================================================
+                    // Remove user from localstorage in any case
+                    // ==========================================================================
+                    $window.localStorage.removeItem('user');
+
+                    currentuser = null;
+
+                    $http.post(constants.logouturl).success(function(data, status){
+                        // ==========================================================================
+                        // If the was no logged in user we are good anyway
+                        // ==========================================================================
+                        if (status === 403 ) {
+                            resolve();
+                            return;
+                        }
+
+                        // ==========================================================================
+                        // We are logged out
+                        // ==========================================================================
+                        if (angular.isObject(data) && data.success ){
+                            resolve();
+                            return;
+                        }
+
+                        reject({
+                            name: 'unkown',
+                            message: 'Logging out from server failed'
+                        });
+
+                    }).error(function(data) {
+                        if (data && data.error) {
+                            reject(data.error);
+                        } else {
+                            reject({
+                                name: 'unkown',
+                                message: 'Logging out from server failed. Because of a Server error'
+                            });
+                        }
+                    });
+                });
             },
             currentUser: function(){
                 return currentuser;
