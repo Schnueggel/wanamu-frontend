4c59f0d140ad64d54956404ab3d5a639cd6d2645
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-26 23:02:44 +0200

    Added directive to hide elements if user is authed

--- src/app/services/auth.js
+++ src/app/services/auth.js
@@ -1,31 +1,32 @@
 /**
- * Created by Christian on 5/24/2015.
+ * This Module create a Service named auth and a directive named tdIsAuth
+ * @param {Object} ngModule
  */
+module.exports = function (ngModule) {
 
-module.exports = function(ngModule){
+    /**
+     * Service for handling auth
+     */
+    ngModule.factory('auth', ['$q', '$http', '$window', 'constants', function ($q, $http, $window, constants) {
 
-    ngModule.factory('auth', ['$q', '$http', '$window','constants', function($q, $http, $window, constants){
-
-        var currentuser =  $window.localStorage.getItem('user');
+        var currentuser = $window.localStorage.getItem('user');
 
         return {
-            login: function(username, password) {
+            login: function (username, password) {
                 /**
                  * @return {Promise}
                  */
-                return $q(function(resolve, reject){
+                return $q(function (resolve, reject) {
 
                     $http.post(constants.loginurl, {
-                        username: username,
-                        password: password
-                    }).success(function(data, status){
+                        username: username, password: password
+                    }).success(function (data, status) {
                         console.log(data);
                         var isObject = angular.isObject(data);
 
-                        if (!isObject || !angular.isArray(data.data) || data.data.length !== 1 || !angular.isNumber(data.data[0].id)){
+                        if (!isObject || !angular.isArray(data.data) || data.data.length !== 1 || !angular.isNumber(data.data[0].id)) {
                             reject({
-                                name: 'unkown',
-                                message: 'Invalid data received from server'
+                                name: 'unkown', message: 'Invalid data received from server'
                             });
                             return;
                         } else {
@@ -36,18 +37,16 @@ module.exports = function(ngModule){
                             resolve(currentuser);
                         }
 
-                    }).error(function(data, status) {
+                    }).error(function (data, status) {
                         if (status === 401 || status == 403) {
                             reject({
-                                name: 'AuthError',
-                                message: 'Login failed. Please check your login data'
+                                name: 'AuthError', message: 'Login failed. Please check your login data'
                             });
                         } else if (status === 500) {
                             reject({
-                                name: 'ServerError',
-                                message: 'The anwser from the server was invalid. Please try again'
+                                name: 'ServerError', message: 'The anwser from the server was invalid. Please try again'
                             });
-                        } else if( data && data.error) {
+                        } else if (data && data.error) {
                             reject(data.error);
                         } else {
                             reject({
@@ -57,13 +56,12 @@ module.exports = function(ngModule){
                         }
                     });
                 });
-            },
-            /**
+            }, /**
              * Logsout user
              * @returns {Promise}
              */
-            logout: function() {
-                return $q(function(resolve, reject){
+            logout: function () {
+                return $q(function (resolve, reject) {
 
                     // ==========================================================================
                     // Remove user from localstorage in any case
@@ -72,11 +70,11 @@ module.exports = function(ngModule){
 
                     currentuser = null;
 
-                    $http.post(constants.logouturl).success(function(data, status){
+                    $http.post(constants.logouturl).success(function (data, status) {
                         // ==========================================================================
                         // If the was no logged in user we are good anyway
                         // ==========================================================================
-                        if (status === 403 ) {
+                        if (status === 403) {
                             resolve();
                             return;
                         }
@@ -84,37 +82,79 @@ module.exports = function(ngModule){
                         // ==========================================================================
                         // We are logged out
                         // ==========================================================================
-                        if (angular.isObject(data) && data.success ){
+                        if (angular.isObject(data) && data.success) {
                             resolve();
                             return;
                         }
 
                         reject({
-                            name: 'unkown',
-                            message: 'Logging out from server failed'
+                            name: 'unkown', message: 'Logging out from server failed'
                         });
 
-                    }).error(function(data) {
+                    }).error(function (data) {
                         if (data && data.error) {
                             reject(data.error);
                         } else {
                             reject({
-                                name: 'unkown',
-                                message: 'Logging out from server failed. Because of a Server error'
+                                name: 'unkown', message: 'Logging out from server failed. Because of a Server error'
                             });
                         }
                     });
                 });
-            },
-            currentUser: function(){
+            }, currentUser: function () {
                 return currentuser;
-            },
-            isLoggedIn: function() {
-                if (currentuser){
+            }, isLoggedIn: function () {
+                if (currentuser) {
                     return true;
                 }
                 return false;
             }
         }
+    }]);
+
+    /**
+     * Depending on auth state this directive hides or shows an linked element
+     */
+    ngModule.directive('tdIsAuth', [ 'auth', function ( auth ) {
+
+        function link ( $scope, $element, attributes ) {
+            /**
+             * Hides or shows the element
+             * @param {boolean} show
+             */
+            function visisible(show){
+                if (show) {
+                    $element.removeClass('ng-hide');
+                } else {
+                    $element.addClass('ng-hide');
+                }
+            }
+
+            visisible(auth.isLoggedIn());
+
+            $scope.$watch(auth.isLoggedIn,
+                function(newValue, oldValue){
+                    visisible(newValue);
+                }
+            );
+        }
+
+        /**
+         * Hides or shows the element
+         * @param {boolean} show
+         */
+        function visisible(show){
+            if (show) {
+                element.hide();
+            } else {
+                element.show();
+            }
+        }
+
+        // Return the directive configuration.
+        return {
+            link: link,
+            restrict: "A"
+        };
     }]);
 };
