a39118a1a56e4757d47bef511f440e8e7fbf9d49
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-16 04:27:09 +0200

    removed Error module

--- src/app/errors/Errors.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Christian on 06.06.2015.
- */
-
-import AuthErrorModule = require('./AuthError');
-import InvalidResponseErrorModule = require('./InvalidResponseDataError');
-
-export var AuthError = AuthErrorModule.AuthError;
-export var InvalidResponseDataError = InvalidResponseErrorModule.InvalidResponseDataError;
