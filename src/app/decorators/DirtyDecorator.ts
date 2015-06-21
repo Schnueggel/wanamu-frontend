d4d4759023472f4454d6390915e6be1cbd91104a
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-21 20:33:31 +0200

    Fixed order of calling set

--- src/app/decorators/DirtyDecorator.ts
+++ src/app/decorators/DirtyDecorator.ts
@@ -1,12 +1,13 @@
 
-export function dirty (target : wanamu.IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : any {
+export function Dirty (target : wanamu.IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : any {
     let setter = descriptor.set;
     let getter = descriptor.get;
 
     descriptor.set = function(val : any) {
         if (getter() !== val) {
+            setter.call(target, val);
             target.dirty = true;
         }
-        setter.call(this, val);
-    }
+    };
+    return descriptor;
 }
