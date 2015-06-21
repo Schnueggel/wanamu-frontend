5dbb10aa04f05fb31b79792f7a1c14acd6d24dc8
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-21 20:43:33 +0200

    Made DirtyDecarots work for normal methods

--- src/app/decorators/DirtyDecorator.ts
+++ src/app/decorators/DirtyDecorator.ts
@@ -1,13 +1,21 @@
+import _ = require('lodash');
 
-export function Dirty (target : wanamu.IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : any {
-    let setter = descriptor.set;
-    let getter = descriptor.get;
-
-    descriptor.set = function(val : any) {
-        if (getter() !== val) {
-            setter.call(target, val);
+export function Dirty (target : wanamu.IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : void {
+    if (_.isFunction(descriptor.value)) {
+        let orgmethod = descriptor.value;
+        descriptor.value = (...args: any[]) => {
+            orgmethod.apply(target, args);
             target.dirty = true;
         }
-    };
-    return descriptor;
+    } else {
+        let setter = descriptor.set;
+        let getter = descriptor.get;
+
+        descriptor.set = function(val : any) {
+            if (getter() !== val) {
+                setter.call(target, val);
+                target.dirty = true;
+            }
+        };
+    }
 }
