08d09f4c595b10962cb5799c7978abc862874afa
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-22 22:44:12 +0200

    Added OnDirtyDecorator

--- src/app/decorators/DirtyDecorator.ts
+++ src/app/decorators/DirtyDecorator.ts
@@ -1,21 +1,38 @@
 import _ = require('lodash');
 
-export function Dirty (target : wanamu.IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : void {
-    if (_.isFunction(descriptor.value)) {
+interface  IDirty extends wanamu.IDirty {
+    __onDirty : Array<Function>
+}
+
+export function Dirty (target : IDirty, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : void {
+
+    if (_.isFunction(descriptor.value)) {   //If its a normal method we go here
         let orgmethod = descriptor.value;
-        descriptor.value = (...args: any[]) => {
-            orgmethod.apply(target, args);
-            target.dirty = true;
+        descriptor.value = function (...args: any[]) {
+            orgmethod.apply(this, args);
+            this.dirty = true;
+            if (_.isArray(this.__onDirty)) {
+                this.__onDirty.forEach((v: Function) => v.call(this));
+            }
         }
-    } else {
+
+    } else {   //If its a setter we go here
         let setter = descriptor.set;
         let getter = descriptor.get;
 
         descriptor.set = function(val : any) {
             if (getter() !== val) {
-                setter.call(target, val);
-                target.dirty = true;
+                setter.call(this, val);
+                this.dirty = true;
+                if (_.isArray(this.__onDirty)) {
+                    this.__onDirty.forEach((v : Function) => v.call(this));
+                }
             }
         };
     }
+}
+
+export function OnDirty <T extends IDirty> (target : T, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
+    target.__onDirty = target.__onDirty || [];
+    target.__onDirty.push(descriptor.value);
 }
