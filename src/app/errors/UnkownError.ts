d10ebc3c29b7f4278573cb100d29f9ae42fa5598
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-27 14:12:32 +0200

    Changed message

--- src/app/errors/UnkownError.ts
+++ src/app/errors/UnkownError.ts
@@ -2,5 +2,5 @@ import { BaseError } from  './BaseError';
 
 export class UnkownError extends BaseError {
     public name: string = 'UnkownError';
-    public message : string = 'Undefined error happend';
+    public message : string = 'An undefined error happend';
 }
