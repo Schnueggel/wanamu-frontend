8d4fd0f7a5e618d654a71cfe2ff77a088ebc27a4
Christian Steinmann <christian.steinmann77@gmail.com>
2015-06-27 11:40:28 +0200

    Added typescript tsd.d.ts

---
+++ typings/tsd.d.ts
@@ -0,0 +1,10 @@
+
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angular-material/angular-material.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="requirejs/require.d.ts" />
