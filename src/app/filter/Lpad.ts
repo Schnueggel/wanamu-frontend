6ffbc74f4956e210718aa2adb1a86c261b8412eb
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-26 16:17:36 +0200

    Added documentation

--- src/app/filter/Lpad.ts
+++ src/app/filter/Lpad.ts
@@ -1,9 +1,30 @@
+/**
+ * Pads a string on the left site with the pad string To the max length
+ *
+ * E.g:
+ * input: test
+ * len: 10
+ * pad: +
+ * result: ++++++test
+ *
+ * If pad string is not a char the resulting string will be longer then len
+ *
+ * If input is longer then len. Input will be returned
+ *
+ * Default pad string is 0
+ * @returns {function(any, number, string): (string)}
+ */
 export function lpad() : Function {
     return function(input : any, len : number, pad : string){
+        // Whatever input comes we need a string, on null or undefined we fail but that is good
         input = input.toString();
-        if(input.length >= len) return input;
-        else{
+        // If input is longer then len, nothing to do
+        if(input.length >= len) {
+            return input;
+        } else {
+            // Check is pad string is given else take 0
             pad = (pad || 0).toString();
+            // We use arry join to concat the pad string to its total length
             return new Array(1 + len - input.length).join(pad) + input;
         }
     };
